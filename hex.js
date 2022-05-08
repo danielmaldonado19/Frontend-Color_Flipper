bfcabccde153a625eb81591d79ed513da05535c3
Daniel Maldonado <damaldy19@hotmail.com>
2022-05-08 17:35:43 -0500

    Testing for fn for generating hex colors

--- hex.js
+++ hex.js
@@ -8,16 +8,39 @@ const color = document.querySelector(".color");
 
 btn.addEventListener('click', () => {
 
-    const rdmParam = randomNumber(hexElements.length);
-    console.log(rdmParam);
+    /*const rdmParam0 = randomNumber(hexElements.length);
+    let hexChar0 = (hexElements[rdmParam0]).toString();
 
-    let hexParam0 = hexElements[rdmParam];
-    console.log(hexParam0);
-    /*document.body.style.backgroundColor = 
-    defaultColors[rdmNumber];
+    const rdmParam1 = randomNumber(hexElements.length);
+    let hexChar1 = (hexElements[rdmParam1]).toString();
+
+    const rdmParam2 = randomNumber(hexElements.length);
+    let hexChar2 = (hexElements[rdmParam2]).toString();
+
+    const rdmParam3 = randomNumber(hexElements.length);
+    let hexChar3 = (hexElements[rdmParam3]).toString();
+
+    const rdmParam4 = randomNumber(hexElements.length);
+    let hexChar4 = (hexElements[rdmParam4]).toString();
+
+    const rdmParam5 = randomNumber(hexElements.length);
+    let hexChar5 = (hexElements[rdmParam5]).toString();*/
     
+    /*let text = document.body.style.backgroundColor = 
+    hexChar0 + hexChar1 + hexChar2 + hexChar3 + hexChar4
+    + hexChar5;
+    
+    console.log(text);*/
 
-    color.textContent = defaultColors[rdmNumber];*/
+    hexParam = [];
+
+    for(i = 0; i <= 5; i++){
+        rdmParam = randomNumber(hexElements.length)
+        hexParam.push(rdmParam);
+        console.log(rdmParam)
+    }
+
+    color.textContent = hexElements[rdmParam];
     
 });
 

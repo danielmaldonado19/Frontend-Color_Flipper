101f70bf99f4f4780838557ac2b30c43c546c08d
Daniel Maldonado <damaldy19@hotmail.com>
2022-05-08 11:30:30 -0500

    btn initial st up

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hola");
+const defaultColors = ["navy", "purple","rgba(22,160,133)",
+ "#cd5c5c"];
+
+ const btn = document.getElementById("btn");
+//btn
+
+const color = document.querySelector(".color");
+/*querySelector method returns the FIRST element that 
+matches with the method string parameter(CSS type
+selector). In this case, the param has a "." because
+thats how we call it in CSS.*/
+
+btn.addEventListener('click', () => {
+    console.log(document.body);
+})
